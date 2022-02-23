c5143bfb983a1b5732cd980bd02e527c11fbe243
Jeffrey Immanuel <94883101+Jeffmanjones@users.noreply.github.com>
2022-02-23 10:28:37 +0000

    p4

--- p4original.c
+++ p4original.c
@@ -8,5 +8,26 @@ int input()
 }
 int find_fibo(int n)
 {
-  
+  int p1=0;
+  int p2=1;
+  int temp;
+  for(int i=0;i<n;i++)
+  {
+    temp=p2;
+    p2=p1+p2;
+    p1=temp;
+  }
+  return p1;
+}
+void output(int p1)
+{
+  printf("%d",p1);
+}
+int main()
+{
+  int n;
+  n=input();
+  int fib=find_fibo(n);
+  output(fib);
+  return 0;
 }
